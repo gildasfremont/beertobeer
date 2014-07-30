861e0ff8eae0a259f72abf2850d70c44f8b39ef2
Ilan Coulon <ilan.coulon@gmail.com>
2014-07-30 16:47:04 +0200

    Installation de EntityAuditBundle
    Ce bundle permet le versionning de toutes les modifications pour
    permettre aux administrteurs de garder un historique et de pouvoir
    revenir aux anciennes versions si besoin

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new FOS\RestBundle\FOSRestBundle(),
+            new SimpleThings\EntityAudit\SimpleThingsEntityAuditBundle(),
             
             new BeerToBeer\CoreBundle\BeerToBeerCoreBundle(),
         );
