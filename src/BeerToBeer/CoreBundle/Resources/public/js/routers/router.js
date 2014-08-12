378fb034f0ca33006d50967ea2190214ebc04388
Ilan Coulon <ilan.coulon@gmail.com>
2014-08-12 19:24:04 +0200

    Fonction géolocalisation

--- src/BeerToBeer/CoreBundle/Resources/public/js/routers/router.js
+++ src/BeerToBeer/CoreBundle/Resources/public/js/routers/router.js
@@ -3,12 +3,18 @@ $(function() {
 	var Router = Backbone.Router.extend({
 	  routes: {
 	    "search/:adress": "search",
+	    "gps": "gps"
 	  },
 
 	  search: function(adress){
 	    console.log("Search Route requested.");
 	    app.AppView.BusinessesView.search(adress);
 	  },
+
+	  gps: function() {
+	  	console.log("GPS Route requested.");
+	  	app.AppView.BusinessesView.gps();
+	  }
 	});
 
 	console.log("Init Router");
--- src/BeerToBeer/CoreBundle/Resources/public/js/views/businesses.js
+++ src/BeerToBeer/CoreBundle/Resources/public/js/views/businesses.js
@@ -11,9 +11,40 @@ app.BusinessesView = Backbone.View.extend({
 
     search: function(adress) {
         console.log("Adresse : "+ adress);
-        $('#searchInput').val(adress);
         this.$el.html(_.template($('#businessList').html()));
+        $('#searchInput').val(adress);
         this.collection.fetch({reset: true, data: {latitude: 48.823549172, longitude: 2.302681803}});
+    },
+
+    gps: function() {
+        this.$el.html(_.template($('#businessList').html()));
+        $('#searchInput').val("Ma position");
+
+        var errorMessage = "";
+        if(navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                app.AppView.BusinessesView.collection.fetch({reset: true, data: {latitude: position.coords.latitude, longitude: position.coords.longitude}});
+            }, function (error) {
+                switch(error.code) {
+                    case error.TIMEOUT:
+                        errorMessage = "Vous avez mis trop de temps à nous donner votre position";
+                    break;
+                    case error.PERMISSION_DENIED:
+                        errorMessage = "Vous devez nous donner la permission de vous localiser";
+                    break;
+                    case error.POSITION_UNAVAILABLE:
+                        errorMessage = "Votre position n’a pu être déterminée";
+                    break;
+                    case error.UNKNOWN_ERROR:
+                        errorMessage = "Erreur inconnue lors de votre géolocalisation";
+                    break;
+                }
+            });
+        } else {
+            errorMessage = "Votre navigateur ne semble pas compatible avec la géolocalisation";
+        }
+        if (errorMessage != "")
+            alert(errorMessage);
     },
 
     render: function() {
