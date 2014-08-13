a1d2d1e21fceaf9ae6a021a372317a51ac8e7892
Ilan Coulon <ilan.coulon@gmail.com>
2014-08-13 12:10:58 +0200

    Ajout des Fixtures Horaires

---
+++ src/BeerToBeer/CoreBundle/DataFixtures/ORM/Horaires.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace BeerToBeer\CoreBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use BeerToBeer\CoreBundle\Entity\Horaire;
+
+class Horaires implements FixtureInterface, OrderedFixtureInterface
+{
+  // Dans l'argument de la méthode load, l'objet $manager est l'EntityManager
+  public function load(ObjectManager $manager)
+  {
+    $items = array(
+        array(
+          "business" => "Le Timbre Poste",
+          "jour" => 3,
+          "ouverture" => new \DateTime("9:00"),
+          "duree" => new \DateTime("14:00"),
+          "happyHour" => false
+        ),
+        array(
+          "business" => "Le Timbre Poste",
+          "jour" => 3,
+          "ouverture" => new \DateTime("16:00"),
+          "duree" => new \DateTime("2:00"),
+          "happyHour" => true
+        ),
+      );
+
+    foreach($items as $i => $item)
+    {
+      $liste_items[$i] = new Horaire();
+      $liste_items[$i]->setJour($item["jour"]);
+      $liste_items[$i]->setOuverture($item["ouverture"]);
+      $liste_items[$i]->setDuree($item["duree"]);
+      $liste_items[$i]->setHappyHour($item["happyHour"]);
+
+      $business = $manager->getRepository('BeerToBeerCoreBundle:Business')->findBy(array("nom" => $item["business"]));
+      $business = $business[0];
+      $liste_items[$i]->setBusiness($business);
+
+      $manager->persist($liste_items[$i]);
+    }
+
+    // On déclenche l'enregistrement
+    $manager->flush();
+  }
+  public function getOrder()
+  {
+      return 4;
+  }
+}
