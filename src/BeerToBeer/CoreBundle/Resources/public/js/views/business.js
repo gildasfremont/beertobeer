de9a7211b4116a4b6435fbda2479511226ef7f78
Ilan Coulon <ilan.coulon@gmail.com>
2014-08-17 18:53:50 +0200

    Début rendement bières

--- src/BeerToBeer/CoreBundle/Resources/public/js/views/business.js
+++ src/BeerToBeer/CoreBundle/Resources/public/js/views/business.js
@@ -5,6 +5,7 @@ app.BusinessView = Backbone.View.extend({
     className: 'row business',
     template: _.template( $( '#business' ).html() ),
     templateFull: _.template( $( '#fullBusiness' ).html() ),
+    templateBeer: _.template( $( '#beer' ).html() ),
 
     events: {
     	"click": "fullBusiness"
@@ -18,6 +19,14 @@ app.BusinessView = Backbone.View.extend({
         return this;
     },
 
+    renderBeers: function(beers) {
+        var html = "";
+        _.each(beers, function(beer) {
+            html += this.templateBeer(beer);
+        });
+        return html;
+    },
+
     fullBusiness: function(e) {
     	location.href = "#business/"+ this.model.get('id');
     },
@@ -25,6 +34,6 @@ app.BusinessView = Backbone.View.extend({
     renderFull: function() {
         //this.model.beers = new app.Beer();
         //this.model.beers.fetch({data: {businessId: this.model.id}});
-    	app.AppView.BusinessesView.$el.html( this.templateFull( this.model.attributes ) );
+    	app.AppView.BusinessesView.$el.html( this.templateFull( this ) );
     }
 });
