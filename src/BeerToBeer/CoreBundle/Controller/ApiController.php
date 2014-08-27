f2b4daad4e846d0d22119e6d7767e45dab942bc2
Ilan Coulon <ilan.coulon@gmail.com>
2014-08-27 11:49:06 +0200

    Suppression actions Beers inutiles

--- src/BeerToBeer/CoreBundle/Controller/ApiController.php
+++ src/BeerToBeer/CoreBundle/Controller/ApiController.php
@@ -33,34 +33,6 @@ class ApiController extends Controller
 		return $response->setData($results);
     }
 
-    public function beerAction() {
-        $request = $this->getRequest();
-
-        if ($request->query->get('businessId') != null)
-            return $this->getBeersWithBusinessAction($request->query->get('businessId'));
-
-        throw new HttpException(404, "Page introuvable.");
-    }
-
-    public function getBeersWithBusinessAction($businessId)
-    {
-        if (!is_numeric($businessId)) {
-            // Throw 400 BAD REQUEST puisqu'il manque des informations ou qu'elles sont mal données
-            throw new HttpException(400, "Vous devez fournir l'ID d'un établissement.");
-        }
-
-        $repoBusiness = $this->getDoctrine()->getManager()->getRepository('BeerToBeerCoreBundle:Business');
-
-        $business = $repoBusiness->find(intval($businessId));
-
-        $repoBeerBusiness = $this->getDoctrine()->getManager()->getRepository('BeerToBeerCoreBundle:BeerBusiness');
-
-        $results = $repoBeerBusiness->getBeersWithBusiness($business);
-
-        $response = new JsonResponse();
-        return $response->setData($results);
-    }
-
     public function getBusinessFromIdAction($id) {
         $repoBusiness = $this->getDoctrine()->getManager()->getRepository('BeerToBeerCoreBundle:Business');
 
